9c6a774b540a20aae1cc77cf9fcf0443b1823e3a
ルイタイゼン <abuaziscorp@gmail.com>
2024-06-16 22:59:46 +0700

    Added ContentItem, Netflix provider and update Dashboard

--- src/features/dashboard/components/ContentItem/index.tsx
+++ src/features/dashboard/components/ContentItem/index.tsx
@@ -1,3 +1,32 @@
+import {
+  Box,
+  Card,
+  CardBody,
+  Flex,
+  Heading,
+  Text,
+  Spacer,
+} from "@chakra-ui/react";
+
 export default function ContentItem() {
-    
+  return (
+    <Card>
+      <CardBody>
+        <Flex flexDirection="row" alignItems="center">
+          <Box>
+            <Heading size="xs" textTransform="uppercase">
+              Movie Name
+            </Heading>
+            <Text pt="2" fontSize="sm">
+              View a summary of your currently movie watched.
+            </Text>
+          </Box>
+          <Spacer />
+          <Text color="blue.600" fontSize="2xl">
+            40:22
+          </Text>
+        </Flex>
+      </CardBody>
+    </Card>
+  );
 }
--- src/features/dashboard/index.tsx
+++ src/features/dashboard/index.tsx
@@ -1,62 +1,35 @@
-import {
-  Box,
-  Button,
-  ButtonGroup,
-  Card,
-  CardBody,
-  Center,
-  Flex,
-  Heading,
-  Spacer,
-  Text,
-} from "@chakra-ui/react";
+import { Box, Button, Center, Flex, Heading, Spacer } from "@chakra-ui/react";
+import ContentItem from "./components/ContentItem";
 
 export default function Dashboard() {
-  const isSIMKLAuthorized = false;
+  const isSIMKLAuthorized = true;
+  const isWatching = true;
 
   return (
     <Box width="40rem" p={6}>
       <Flex minWidth="max-content" alignItems="center" gap="2">
-        <Box p="2">
-          <Heading size="lg">Momate</Heading>
-        </Box>
+        <Heading p={2} size="lg">
+          Momate
+        </Heading>
         <Spacer />
-        <ButtonGroup gap="2">
-          <Button
-            colorScheme={isSIMKLAuthorized ? "green" : "blue"}
-            isDisabled={isSIMKLAuthorized}
-          >
-            {isSIMKLAuthorized ? "Username SIMKL" : "Authorize SIMKL Account"}
-          </Button>
-        </ButtonGroup>
+        <Button
+          gap="2"
+          colorScheme={isSIMKLAuthorized ? "green" : "blue"}
+          isDisabled={isSIMKLAuthorized}
+        >
+          {isSIMKLAuthorized ? "Username SIMKL" : "Authorize SIMKL Account"}
+        </Button>
       </Flex>
       {isSIMKLAuthorized ? (
         <>
           <Box p="2" marginY="6">
             <Center>
-              <Heading size="md" textColor="green">
-                Currently Watching
+              <Heading size="md" textColor={isWatching ? "green" : "grey"}>
+                {isWatching ? "Currently Watching" : "Nothing To Watch"}
               </Heading>
             </Center>
           </Box>
-          <Card>
-            <CardBody>
-              <Flex flexDirection="row" alignItems="center">
-                <Box>
-                  <Heading size="xs" textTransform="uppercase">
-                    Movie Name
-                  </Heading>
-                  <Text pt="2" fontSize="sm">
-                    View a summary of your currently movie watched.
-                  </Text>
-                </Box>
-                <Spacer />
-                <Text color="blue.600" fontSize="2xl">
-                  40:22
-                </Text>
-              </Flex>
-            </CardBody>
-          </Card>
+          {isWatching ? <ContentItem /> : <></>}
         </>
       ) : (
         <></>
